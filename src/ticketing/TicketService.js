2ea1cd5a180bf8f16422d1598f88ca10b4b53a37
rpmcloud <ronaldpmathews@icloud.com>
2023-02-10 21:26:04 +0000

    refactor: move max ticket per transaction to a constant

--- src/ticketing/TicketService.js
+++ src/ticketing/TicketService.js
@@ -2,9 +2,11 @@
 import TicketPaymentService from "../thirdparty/paymentgateway/TicketPaymentService.js"
 import SeatReservationService from "../thirdparty/seatbooking/SeatReservationService.js"
 
-// Fixed ticket prices
+// Constants
 const ADULT_TICKET_PRICE = 20
 const CHILD_TICKET_PRICE = 10
+const MAX_TICKETS_PER_TXN = 20
+
 
 // Function to check if the count is a valid and acceptable number
 // any future validation additions can go here
@@ -40,8 +42,8 @@ export default class TicketService {
       // Calculate total tickets which include infants (since it is a ticket category)
       const totalTickets = adult + child + infant
       // Maximum seat allowed per booking is 20. Abort booking if this criteria fails.
-      if(totalTickets > 20){
-        return "Only a maximum of 20 seats could be purchased at a time"
+      if(totalTickets > MAX_TICKETS_PER_TXN){
+        return "Only a maximum of "+ MAX_TICKETS_PER_TXN +" seats could be purchased at a time"
       }
 
       // Seats to be booked only for adult and child categories
